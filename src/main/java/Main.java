8ca4161d891ac59c6fc1eb2e4b471d646708179a
formerFox <formerfox@yandex.ru>
2024-09-01 23:45:05 +0300

    add classes Car, Race, change main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,49 @@
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // по сканеру вышел костыль, у меня не получается сделать это все на одном сканере
+        // вылетает программа, хотелось бы получить консультацию, если возможно
+        Scanner scannerInt = new Scanner(System.in);
+        Scanner scannerLine = new Scanner(System.in);
+
+        int count;
+        Race racer = new Race();
+
+        while(true){
+            System.out.println("Введите количество машин:");
+            if(scannerInt.hasNextInt()){
+                count = scannerInt.nextInt();
+                break;
+            }
+            else {
+                System.out.println("Ошибка ввода! Попробуйте снова!");
+                scannerInt.next();
+            }
+        }
+
+        for(int i = 0; i < count; i++) {
+            System.out.println("Введите название " + (i+1) + "-ой машины:");
+            String nameCar = scannerLine.nextLine();
+
+            int speedCar;
+            while (true) {
+                System.out.println("Введите скорость " + (i+1) + "-ой машины:");
+                if (scannerInt.hasNextInt()) {
+                    speedCar = scannerInt.nextInt();
+                    if( speedCar > 0 && speedCar < 250) {
+                        racer.checkRacer(nameCar, speedCar);
+                        break;
+                    }
+                } else {
+                    System.out.println("Неправильная скорость!");
+                    scannerInt.next();
+                }
+            }
+        }
+        System.out.println("Победитель: " + racer.getRace());
+        scannerInt.close();
+        scannerLine.close();
     }
 }
