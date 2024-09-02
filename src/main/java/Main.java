4b4da7291d89b4a36c70abab89a4f6b984e332e8
formerFox <Formerfox@yandex.ru>
2024-09-02 18:39:31 +0300

    change main: correct use of class Scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,45 +3,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scannerInt = new Scanner(System.in);
-        Scanner scannerLine = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         int count;
         Race racer = new Race();
 
         while(true){
             System.out.println("Введите количество машин:");
-            if(scannerInt.hasNextInt()){
-                count = scannerInt.nextInt();
+            if(scanner.hasNextInt()){
+                count = scanner.nextInt();
+                scanner.nextLine();
                 break;
             }
             else {
                 System.out.println("Ошибка ввода! Попробуйте снова!");
-                scannerInt.next();
+                scanner.next();
             }
         }
 
         for(int i = 0; i < count; i++) {
             System.out.println("Введите название " + (i+1) + "-ой машины:");
-            String nameCar = scannerLine.nextLine();
+            String nameCar = scanner.nextLine();
 
             int speedCar;
             while (true) {
-                System.out.println("Введите скорость " + (i+1) + "-ой машины:");
-                if (scannerInt.hasNextInt()) {
-                    speedCar = scannerInt.nextInt();
+                System.out.println("Введите скорость " + (i+1) + "-ой машины(больше 0 и меньше 250):");
+                if (scanner.hasNextInt()) {
+                    speedCar = scanner.nextInt();
+                    scanner.nextLine();
                     if( speedCar > 0 && speedCar < 250) {
                         racer.checkRacer(nameCar, speedCar);
                         break;
                     }
                 } else {
                     System.out.println("Неправильная скорость!");
-                    scannerInt.next();
+                    scanner.next();
                 }
             }
         }
         System.out.println("Победитель: " + racer.getRace());
-        scannerInt.close();
-        scannerLine.close();
+        scanner.close();
     }
 }
